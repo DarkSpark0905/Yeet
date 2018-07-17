27ba128b0ff642c68fc4b0310058dfed653d88c0
DarkSpark0905 <DarkSpark0905@gmail.com>
2018-07-16 17:26:11 -0700

    Worked on Main and edited Enemy

--- HammerTime/Main.cpp
+++ HammerTime/Main.cpp
@@ -1,4 +1,25 @@
-int main() 
-{
+#include "Enemy.h"
+#include <time.h>
+int main() {
+	srand(clock());
+	int enemyNum = 10;
+	int p_health = 100;
+	int p_damage = 5;
+	Enemy ** enemies = new Enemy *[enemyNum];
+	for (int i = 0; i < enemyNum; i++) {
+		enemies[i] = new Enemy((rand() % 20) + 5, (rand() % 5) + 1);
+	}
+	bool enemiesAlive = true;
+	while (p_health > 0 && enemiesAlive) {
+		bool e = true;
+		for (int i = 0; i > enemyNum; i++) {
+			if (enemies[i] != NULL && p_health > 0) {
+				e = false;
+				std::cout << "Enemies and shiz";
+				//bool alive
+			}
+		}
+	}
+
 	return 0;
 }
